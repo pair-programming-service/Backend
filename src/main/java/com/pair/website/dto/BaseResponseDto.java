e46e7f6e311feb826d7e8238c046b7df7c55bd98
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-04-10 22:29:14 +0900

    Feat : 공통 Response

---
+++ src/main/java/com/pair/website/dto/BaseResponseDto.java
@@ -0,0 +1,29 @@
+package com.pair.website.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseResponseDto<T> {
+    private boolean success;
+    private T data; //제네릭의 변수 선언방법
+    private Error error;
+
+    @AllArgsConstructor
+    @Getter
+    public static class Error {
+        private String code;
+        private String message;
+    }
+
+
+    public static <T> BaseResponseDto<T> success(T data) {
+
+        return new BaseResponseDto<T>(true, data, null);
+    }
+
+    public static <T> BaseResponseDto<T> fail(String code, String message) {
+        return new BaseResponseDto<T>(false, null, new Error(code, message));
+    }
+}
